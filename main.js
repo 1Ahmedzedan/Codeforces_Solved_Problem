aaab9be4ee3f187237107df5c76bd30bf22ffd31
Ahmed Zedan <zedan@gmail.com>
2023-07-07 09:26:48 +0300

    add some comments to js file

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
+// function to get the problems of a particular tag solved by a particular user
 function get_problem(tag , handle){
+
     let req = new XMLHttpRequest();
     req.open('GET', `https://codeforces.com/api/user.status?handle=${handle}`);
     req.send();
+
     req.onload = function(){
+        // create a div to store the problems
         let problems = document.createElement('div') ;
         problems.classList.add('problems') ;
         problems.style.padding = '10px' ;
         document.body.appendChild(problems) ;
-        let data = JSON.parse(req.responseText).result ;
-        let st = new Set() ;
+
+        let data = JSON.parse(req.responseText).result ; // array of objects
+
+        let st = new Set() ; // to store the problems already added
+
         for(let i=0 ; i<data.length ; i++){
+
             let tags = data[i].problem.tags ;
             let contestId = data[i].contestId ;
             let index = data[i].problem.index ;
+            
+            // if the problem is solved and not already added and has the required tag
             if(data[i].verdict==="OK" && !st.has(contestId+index) && tags.includes(tag)){
-                st.add(contestId+index) ;
-                let link = `https://codeforces.com/contest/${contestId}/problem/${index}` ;
+                st.add(contestId+index) ; // add the problem to the set
+
+                let link = `https://codeforces.com/contest/${contestId}/problem/${index}` ; // link to the problem
+
+                // create a div to store the problem name and add it to the problems div
                 let name = data[i].problem.name ;
                 let a_tag = document.createElement('a') ;
                 let a_div = document.createElement('div') ;
@@ -30,6 +43,7 @@ function get_problem(tag , handle){
     }
 }
 
+
 let menu = document.querySelector('.dropdown') ;
 let select = document.querySelector('.select') ;
 let option_menu = document.querySelector('.options') ;
@@ -38,6 +52,8 @@ let select_span = select.querySelector('span') ;
 let arrow_up = document.querySelector('.up') ;
 let arrow_down = document.querySelector('.down') ;
 
+
+// add click event listener to the select div
 select.addEventListener('click' , ()=>{
     if(option_menu.classList.contains('active')){
         option_menu.classList.remove('active') ;
@@ -52,10 +68,14 @@ select.addEventListener('click' , ()=>{
 });
 
 
+
+
 let tag = "" ;
 let handle = "" ; 
 let submit = document.querySelector('.submit') ;
-// console.log(tag.length) ;
+
+
+// add click event listener to all the options 
 options.forEach((option)=>{
     option.addEventListener('click' , ()=>{
         tag = option.innerHTML ;
@@ -66,6 +86,8 @@ options.forEach((option)=>{
     });
 });
 
+
+// add click event listener to the submit button
 submit.addEventListener('click' , ()=>{
     console.log('clicked') ;
     handle = document.querySelector('.handle').value ;
